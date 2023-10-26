67bb0546e16feeb52967f575eba5aa5b0c9714c2
呆奇士 チ <tsaiyuchi@msn.com>
2023-10-27 02:18:39 +0800

    update

--- CToolkitCs.v1_2Core/ContextFlow/ICtkContextFlowRun.cs
+++ CToolkitCs.v1_2Core/ContextFlow/ICtkContextFlowRun.cs
@@ -10,14 +10,8 @@ namespace CToolkitCs.v1_2Core.ContextFlow
     ///     RunLoop call RunOnce call RunSelf(自定)
     ///     RunLoopStart call RunSelf(自定)
     /// </summary>
-    public interface ICtkContextFlowRun : ICtkContextFlow
+    public interface ICtkContextFlowRun : ICtkContextFlowRunStart
     {
-        /*[d20210714] RunOnce, RunLoop 以及 RunLoopStart.
-         * 在介面中只定義各Method存在, 宣告該Method的用途
-         * 但實際用途是實作者決定, 實作者應自行注意如何符合定義.
-         */
-
-        bool CtkCfIsRunning { get; set; }
 
         /// <summary>
         /// 會執行一次特定功能的method
@@ -34,11 +28,5 @@ namespace CToolkitCs.v1_2Core.ContextFlow
         /// <returns></returns>
         int CtkCfRunLoop();
 
-        /// <summary>
-        /// 會持續執行特定功能的method
-        /// 需實作非同步作業, e.q. 開啟一個Thread/Task後離開函式
-        /// </summary>
-        /// <returns></returns>
-        int CtkCfRunLoopStart();
     }
 }
---
+++ CToolkitCs.v1_2Core/ContextFlow/ICtkContextFlowRunStart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CToolkitCs.v1_2Core.ContextFlow
+{
+    /// <summary>
+    /// 建議:
+    ///     RunLoop call RunOnce call RunSelf(自定)
+    ///     RunLoopStart call RunSelf(自定)
+    /// </summary>
+    public interface ICtkContextFlowRunStart : ICtkContextFlow
+    {
+        /*[d20210714] RunOnce, RunLoop 以及 RunLoopStart.
+         * 在介面中只定義各Method存在, 宣告該Method的用途
+         * 但實際用途是實作者決定, 實作者應自行注意如何符合定義.
+         */
+
+        bool CtkCfIsRunning { get; set; }
+
+
+        /// <summary>
+        /// 會持續執行特定功能的method
+        /// 需實作非同步作業, e.q. 開啟一個Thread/Task後離開函式
+        /// </summary>
+        /// <returns></returns>
+        int CtkCfRunLoopStart();
+    }
+}
